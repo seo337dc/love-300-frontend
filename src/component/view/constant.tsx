3c75a63c38eceff3a93852b75512916fb32d6d70
seo337dc <sdc337dc@gmail.com>
2024-11-28 17:41:15 +0900

    feature : 초기 페이지 분리 및 임시 데이터 적용

---
+++ src/component/view/constant.tsx
@@ -0,0 +1,34 @@
+import type { TPatient } from "@/types";
+
+export const TEST_DATA: TPatient[] = [
+  {
+    id: 12345,
+    name: "김환자",
+    status: "신규",
+    sex: "M",
+    age: 63,
+    dong: "ICU",
+    ho: 124,
+    hoDetail: 11,
+  },
+  {
+    id: 12346,
+    name: "김환자",
+    status: "신규",
+    sex: "M",
+    age: 63,
+    dong: "ICU",
+    ho: 124,
+    hoDetail: 11,
+  },
+  {
+    id: 12347,
+    name: "김환자",
+    status: "신규",
+    sex: "M",
+    age: 63,
+    dong: "ICU",
+    ho: 124,
+    hoDetail: 11,
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type TPatient = {
+  id: number;
+  name: string;
+  status: string;
+  sex: string;
+  age: number;
+  dong: string; // ICU
+  ho: number; // 124
+  hoDetail: number; //  11
+};
