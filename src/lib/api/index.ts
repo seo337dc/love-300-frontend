cd99d3982c21868c05507f77bcf20c53b0a5e96e
seo337dc <sdc337dc@gmail.com>
2024-11-30 02:03:08 +0900

    add : 데이터 로딩 컴포넌트, 페이지 로딩 Suspense 추가

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -4,7 +4,7 @@ import type { PaginationResponse, Status, TPatient } from "@/types";
 
 export interface RequestModelPatients {
   _page: number;
-  _size?: number;
+  _limit?: number;
   statuses: Status[];
 }
 
@@ -12,36 +12,69 @@ export interface ResponseModelPaients extends PaginationResponse {
   data: TPatient[];
 }
 
+export interface ResponseModelAll {
+  totalCtn: number;
+  screenedCtn: number;
+  obeservingCtn: number;
+  errorCtn: number;
+  doneCtn: number;
+  dnrCtn: number;
+}
+
 export async function getPatients(
   params: RequestModelPatients
 ): Promise<ResponseModelPaients> {
   try {
-    const { _page, _size = 10 } = params;
+    const { _page, _limit = 10, statuses } = params;
     const queryParams = toQueryString({
       _page,
-      _size,
+      _limit,
       _sort: "admission_dt",
       _order: "desc",
+      status: statuses.join(","),
     });
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const response = await api.get<ResponseModelPaients>(
-      `/patients?status=SCREENED,OBSERVING&_page=1&_limit=10&_sort=admission_dt&_order=desc`
+      `/patients?${queryParams}`
     );
 
-    if (params.statuses) {
-      const filteredData = response.data.filter((patient) =>
-        params.statuses.includes(patient.status)
-      );
+    return response;
+  } catch (error: any) {
+    if (error.type === "http") {
+      console.error(`HTTP Error: ${error.status} - ${error.message}`);
+    } else if (error.type === "network") {
+      console.error(`Network Error: ${error.message}`);
+    } else {
+      console.error("Unknown Error:", error);
+    }
+    throw error;
+  }
+}
 
-      return {
-        ...response,
-        data: filteredData,
+export async function getPatientsAll(): Promise<ResponseModelAll | null> {
+  try {
+    const response = await api.get<ResponseModelPaients>(`/patients-all`);
+
+    if (response.data) {
+      const result: ResponseModelAll = {
+        totalCtn: response.data.length,
+        screenedCtn: response.data.filter((data) => data.status === "SCREENED")
+          .length,
+        obeservingCtn: response.data.filter(
+          (data) => data.status === "OBSERVING"
+        ).length,
+        errorCtn: response.data.filter((data) => data.status === "ERROR")
+          .length,
+        doneCtn: response.data.filter((data) => data.status === "DONE").length,
+        dnrCtn: response.data.filter((data) => data.status === "DNR").length,
       };
+
+      return result;
     }
 
-    return response;
+    return null;
   } catch (error: any) {
     if (error.type === "http") {
       console.error(`HTTP Error: ${error.status} - ${error.message}`);
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,4 @@
+import type { ResponseModelAll } from "@/lib/api";
 import type { FilterStatus, Status } from "@/types";
 
 export const STATUS_LIST: FilterStatus[] = [
@@ -30,4 +31,24 @@ export const statusLabelMap: Record<Status, string> = {
   DONE: "완료",
   ERROR: "오류",
   DNR: "DNR",
+};
+
+// 갯수 관련 노출
+
+export const outputStatusCtn = (type: Status, info: ResponseModelAll) => {
+  switch (type) {
+    case "SCREENED":
+      return info.screenedCtn;
+    case "OBSERVING":
+      return info.obeservingCtn;
+    case "ERROR":
+      return info.errorCtn;
+    case "DONE":
+      return info.doneCtn;
+    case "DNR":
+      return info.dnrCtn;
+
+    default:
+      break;
+  }
 };
