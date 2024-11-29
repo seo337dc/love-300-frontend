a538370294b8b4c953f905f46e1bca89d84ad81e
seo337dc <sdc337dc@gmail.com>
2024-11-29 21:28:18 +0900

    add : 무한스크롤 적용 (로컬 테스트를 위한 strict 비활성화)

--- src/component/view/MainView.tsx
+++ src/component/view/MainView.tsx
@@ -1,25 +1,77 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
 import Table from "@/component/common/Table";
+import { getPatients } from "@/lib/api";
 import type { TPatient } from "@/types";
 
-import { TEST_DATA } from "./constant";
-import { getPatients } from "@/lib/api";
-
 const MainView = () => {
-  const [patients, setPatients] = useState<TPatient[]>(TEST_DATA);
+  const sectionRef = useRef<HTMLElement | null>(null);
+  const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const fetchData = async () => getPatients();
+  const [patients, setPatients] = useState<TPatient[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const fetchData = async (page: number) => {
+    if (isLoading) return;
+
+    try {
+      setIsLoading(true);
+
+      const res = await getPatients({ _page: page });
+
+      if (!!res) {
+        setPatients((prev) => [...prev, ...res.data]);
+        setHasMore(!!res.next); // next가 있으면 true, 없으면 false
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // 무한 스크롤 처리
   useEffect(() => {
-    fetchData();
-  }, []);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting && hasMore && !isLoading) {
+          setPage((prev) => prev + 1); // 페이지 증가
+        }
+      },
+      {
+        root: sectionRef.current || null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, [hasMore, isLoading]);
+
+  // 초기 데이터 로드
+  useEffect(() => {
+    fetchData(page);
+  }, [page]);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Table list={patients} />
+    <div className="flex min-h-screen flex-col px-24 py-4">
+      <section ref={sectionRef} className="w-full h-[90vh] overflow-y-auto">
+        <Table list={patients} />
+        {isLoading && <div className="text-center py-4">로딩 중...</div>}
+        <div ref={observerRef} className="h-10 bg-gray-200" /> {/* 관찰 대상 */}
+      </section>
     </div>
   );
 };
