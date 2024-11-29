2df0b2aca32acae9f0cfc3bf2b192e3dc80a9ea0
seo337dc <sdc337dc@gmail.com>
2024-11-29 21:25:48 +0900

    remove : 불필요한 코드 및 리소스 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import MainView from "@/component/view/MainView";
+import { getPatients } from "@/lib/api";
 
-export default function Home() {
+export default async function Page() {
+  const patients = await getPatients({ _page: 0 }); // 서버사이드 테스트
+
+  if (!patients) {
+    return (
+      <div>
+        <h1>Error</h1>
+      </div>
+    );
+  }
+
+  console.log("patients", patients);
   return <MainView />;
 }
