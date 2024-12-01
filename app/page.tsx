0be0545588bec3e2726fb305f8fc43792f5aa4a2
seo337dc <sdc337dc@gmail.com>
2024-12-01 14:07:51 +0900

    remove : 불필요한 Suspense 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import LoadingPage from "@/component/common/LoadingPage";
+// import { Suspense, use } from "react";
+// import LoadingPage from "@/component/common/LoadingPage";
 import MainView from "@/component/view/MainView";
-import { Suspense, use } from "react";
 
 export default function Page() {
   return (
-    <Suspense fallback={<LoadingPage />}>
-      <MainContent />
-    </Suspense>
+    <MainView />
+    // <Suspense fallback={<LoadingPage />}>
+    //   <MainContent />
+    // </Suspense>
   );
 }
 
-function MainContent() {
-  use(wait(2000));
-  return <MainView />;
-}
+// function MainContent() {
+//   use(wait(2000));
+//   return <MainView />;
+// }
 
-// Helper: 3초 또는 지정한 시간(ms) 동안 대기
-const wait = (ms = 3000) => new Promise((resolve) => setTimeout(resolve, ms));
+// const wait = (ms = 3000) => new Promise((resolve) => setTimeout(resolve, ms));
