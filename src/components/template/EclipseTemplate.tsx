b13dcae48a254d608d7cf2a8501488d792554cfd
seo337dc <sdc337dc@gmail.com>
2024-12-16 17:50:38 +0900

    add : 백그라운 오브젝트 추가

---
+++ src/components/template/EclipseTemplate.tsx
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+type TProps = {
+  children: React.ReactNode;
+};
+const EclipseBackground = ({ children }: TProps) => {
+  return (
+    <Wrapper>
+      <Eclipse1 />
+      <Eclipse2 />
+      <div className="absolute z-10 top-0 left-0 w-full">{children}</div>
+    </Wrapper>
+  );
+};
+
+export default EclipseBackground;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh; /* 화면 전체 크기 */
+  overflow: hidden; /* 화면을 넘어가는 부분 숨기기 */
+`;
+
+const Eclipse1 = styled.div`
+  position: absolute;
+  top: 10%;
+  width: 300px;
+  height: 300px;
+  background-image: url("/images/ellipse-left.png"); /* 첫 번째 이클립스 이미지 경로 */
+  background-size: cover;
+  background-position: center;
+  z-index: 1;
+`;
+
+const Eclipse2 = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  width: 300px;
+  height: 300px;
+  background-image: url("/images/ellipse-right.png"); /* 두 번째 이클립스 이미지 경로 */
+  background-size: cover;
+  background-position: center;
+  z-index: 0;
+`;
