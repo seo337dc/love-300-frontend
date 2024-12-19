9a2d33c73a2c3310864a71909d2a61604778d484
seo337dc <sdc337dc@gmail.com>
2024-12-19 20:59:45 +0900

    fix : 빌드 에러 수정

--- src/components/views/payment/step/Step3.tsx
+++ src/components/views/payment/step/Step3.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SecondPwd from "@/components/common/SecondPwd";
 import Button from "@/components/ui/Button";
 
@@ -6,11 +7,12 @@ type TProps = {
 };
 
 const Step3 = ({ onNext }: TProps) => {
+  const [pwd, setPwd] = useState(["", "", "", ""]);
   return (
     <section className="h-[70vh]">
       <div className="h-[50vh] flex flex-col justify-center items-center">
         <p className="font-medium text-lg mb-5">2차 비밀번호 입력</p>
-        <SecondPwd />
+        <SecondPwd secondPwd={pwd} handlePwd={() => {}} />
       </div>
       <Button onClick={onNext}>다음</Button>
     </section>
